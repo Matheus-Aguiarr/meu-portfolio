2641519bf2b47d6aa1d546b1e0f87fc63e68dbd3
Matheus Aguiar <matheusaguiardealmeida@gmail.com>
2024-10-29 16:04:49 -0300

    add idade

--- scripts/script.js
+++ scripts/script.js
@@ -14,5 +14,18 @@ function toggleMenu() {
     }
 }
 
-/* */
+/* IDADE */
+
+const age = document.getElementById('age');
+
+let born = new Date(2007, 11, 23);
+let thisYear = new Date()
+let myAge = thisYear.getFullYear() - born.getFullYear();
+if (thisYear < new Date(thisYear.getFullYear(), born.getMonth(), born.getDate())) {
+    myAge--;
+}
+
+age.innerHTML = myAge;
+
+
 
