e2fd7dd26b1d25891ab6404d2fd417333651eb0f
Matheus Aguiar <matheusaguiardealmeida@gmail.com>
2024-11-11 21:57:33 -0300

    quando clica na tela sai do menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,19 @@
 const menuHamburguer = document.querySelector('.menu-hamburguer');
+
 menuHamburguer.addEventListener('click', () => {
+    toggleMenu();
+})
+
+const content = document.querySelector('.content');
+content.addEventListener('click', () => {
     toggleMenu();
 })
 
 function toggleMenu() {
     const nav = document.querySelector('.nav-responsive')
     menuHamburguer.classList.toggle('change');
-    
     menuHamburguer.classList.contains('change') ? nav.style.display = 'block' : nav.style.display = 'none';
-
+    
     // if (menuHamburguer.classList.contains('change')) {
     //     nav.style.display = 'block';
     // } else {
